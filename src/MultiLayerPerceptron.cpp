c614c90f7f17316492f13ec22f382facb21b2abd
ShaneWed <shanewedlock03@gmail.com>
2025-06-23 01:28:30 +0100

    Forward pass implemented, appears to be working properly. Should be time for activation functions.

--- src/MultiLayerPerceptron.cpp
+++ src/MultiLayerPerceptron.cpp
@@ -9,4 +9,18 @@ void MultiLayerPerceptron::forward(std::vector<double> inputs) {
         layers.at(0).outputs.at(i) = inputs.at(i);
         std::cout << layers.at(0).outputs.at(i) << std::endl;
     }
+    double weightSum = 0;
+    for (int l = 1; l < layers.size(); l++) { // Each layer
+        for (int i = 0; i < layers.at(l).numOfNeurons; i++) { // This layer's neurons
+            weightSum = 0;
+            for (int j = 0; j < layers.at(l - 1).numOfNeurons; j++) { // Previous layer's neurons
+                weightSum += layers.at(l - 1).outputs.at(j) * layers.at(l).weights.at(i);
+            }
+            weightSum += layers.at(l).biases.at(i);
+            layers.at(l).weights.at(i) = weightSum;
+            // Add activation function
+            std::cout << weightSum << std::endl;
+        }
+    }
+    std::cout << "Forward pass complete!" << std::endl;
 }
